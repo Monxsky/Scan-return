3a7bc2534c4c61e15fc8995e11d379c2824ffaca
Monxsky <monxsky6@gmail.com>
2026-05-12 17:22:44 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,10 +4,6 @@ const SUPABASE_URL =
 const SUPABASE_KEY =
 "sb_publishable_MBnzcusAZqrkGnAhdDkQYA_ilUyr8mW";
 
-const TABLE_NAME =
-"scan_awb";
-"retur_manifest";
-
 const supabase =
   window.supabase.createClient(
     SUPABASE_URL,
