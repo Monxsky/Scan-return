24223d133a8ac7e38145e04c3cff9b1b1cb2810b
Monxsky <monxsky6@gmail.com>
2026-05-12 13:19:02 +0700

    Update filter.js

--- js/filter.js
+++ js/filter.js
@@ -1,3 +1,4 @@
+
 async function filterByDate(
   tableName,
   startDate,
@@ -5,7 +6,7 @@ async function filterByDate(
 ) {
 
   const res = await fetch(
-    `${SUPABASE_URL}/rest/v1/${tableName}?select=*&waktu=gte.${startDate}T00:00:00&waktu=lte.${endDate}T23:59:59&order=waktu.desc`,
+    `${SUPABASE_URL}/rest/v1/${tableName}?select=*&waktu=gte.${startDate}T00:00:00&waktu=lt.${endDate}T23:59:59&order=waktu.desc`,
     {
       headers: {
         apikey: SUPABASE_KEY,
@@ -15,4 +16,25 @@ async function filterByDate(
   );
 
   return await res.json();
+
+}
+
+
+// khusus inbound
+async function filterTanggal() {
+
+  const start =
+    document.getElementById("startDate").value;
+
+  const end =
+    document.getElementById("endDate").value;
+
+  const data = await filterByDate(
+    "scan_awb",
+    start,
+    end
+  );
+
+  renderTable(data);
+
 }
