89a60e21e3cc5f23d6cf01645bcf74477952a69c
Monxsky <monxsky6@gmail.com>
2026-05-14 13:19:01 +0700

    Update pagination.js

--- js/pagination.js
+++ js/pagination.js
@@ -1,5 +1,52 @@
 const limit = 50;
 
+async function loadPage({
+
+  page,
+  table,
+  tbodyId,
+  renderRow
+
+}) {
+
+  currentPage = page;
+
+  const from =
+    (currentPage - 1) * limit;
+
+  const to =
+    from + (limit - 1);
+
+  const { data, error } = await client
+    .from(table)
+    .select("*")
+    .range(from, to);
+
+  const tbody =
+    document.getElementById(tbodyId);
+
+  tbody.innerHTML = "";
+
+  if (!data || data.length === 0) {
+
+    tbody.innerHTML = `
+      <tr>
+        <td colspan="5">
+          Belum ada data
+        </td>
+      </tr>
+    `;
+
+    return;
+  }
+
+  data.forEach(item => {
+
+    tbody.innerHTML += renderRow(item);
+
+  });
+}
+
 async function setupPagination({
 
   table,
