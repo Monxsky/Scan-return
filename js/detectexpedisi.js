26321559aa7a5c617ff7205f173d98298578cefe
Monxsky <monxsky6@gmail.com>
2026-05-15 11:11:49 +0700

    Update detectexpedisi.js

--- js/detectexpedisi.js
+++ js/detectexpedisi.js
@@ -1,9 +1,28 @@
-import { expedisiRules }
-from "./expedisi.js";
+// import { expedisiRules }
+// from "./expedisi.js";
 
-export function detectExpedisi(resi) {
+// export function detectExpedisi(resi) {
 
-  resi = resi.toUpperCase();
+//   resi = resi.toUpperCase();
+
+//   for (const item of expedisiRules) {
+
+//     if (item.test(resi)) {
+//       return item.nama;
+//     }
+
+//   }
+
+//   return "Lainnya";
+// }
+function detectExpedisi(resi) {
+
+  if (!resi)
+    return "Unknown";
+
+  resi = resi
+    .trim()
+    .toUpperCase();
 
   for (const item of expedisiRules) {
 
@@ -15,3 +34,6 @@ export function detectExpedisi(resi) {
 
   return "Lainnya";
 }
+
+window.detectExpedisi =
+detectExpedisi;
