2e5aa7962f0af9f586ac7e85abdb33ae3bdbb5b3
Monxsky <monxsky6@gmail.com>
2026-06-08 17:35:11 +0700

    Update sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -15,4 +15,41 @@ async function loadSidebar() {
     .innerHTML = html;
 }
 
+async function loadSidebar() {
+
+  const response =
+    await fetch(
+      "../component/sidebar.html"
+    );
+
+  const html =
+    await response.text();
+
+  document
+    .getElementById(
+      "sidebar-container"
+    )
+    .innerHTML = html;
+
+  // dropdown
+  document
+    .querySelectorAll(".dropdown-btn")
+    .forEach(btn => {
+
+      btn.addEventListener(
+        "click",
+        () => {
+
+          btn.nextElementSibling
+            .classList.toggle(
+              "active"
+            );
+
+        }
+      );
+
+    });
+
+}
+
 loadSidebar();
