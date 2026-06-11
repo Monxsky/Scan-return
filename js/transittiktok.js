74834cc66ca01be14cf39c6e0033d577feb1f5ab
Monxsky <monxsky6@gmail.com>
2026-06-11 23:21:58 +0700

    Update transittiktok.js

--- js/transittiktok.js
+++ js/transittiktok.js
@@ -1,11 +1,32 @@
-function isTransitToday(order){
-  const transitDate =
-  getBatasKirim(order);
+// function isTransitToday(order){
+//   const transitDate =
+//   getBatasKirim(order);
 
-  const today =
-  new Date()
-  .toISOString()
-  >split("T")[0];
+//   const today =
+//   new Date()
+//   .toISOString()
+//   >split("T")[0];
 
-  return transitDate === today;
+//   return transitDate === today;
+// }
+
+async function loadTransitShopee() {
+
+  const { data, error } =
+    await client
+      .from("order_list")
+      .select("*")
+      .eq("ekspedisi", "SPX");
+
 }
+
+const transitOrders =
+  data.filter(order => {
+
+    return isTransitToday(order);
+
+  });
+
+  renderTransitTable(
+  transitOrders
+);
