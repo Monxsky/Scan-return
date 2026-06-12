93f6d596bcdbc520dfc7df4b20f5630b7b792ba8
Monxsky <monxsky6@gmail.com>
2026-06-12 16:27:59 +0700

    Create transitshopee.js

---
+++ js/transitshopee.js
@@ -0,0 +1,114 @@
+async function loadOrderListTikTok() {
+
+  const { data, error } =
+    await client
+    .from("order_list")
+    .select("*")
+    .in("ekspedisi", [
+      "SPX",
+      "SPX Express",
+      "J&T Cargo"
+    ]);
+
+
+  const transitData =
+  data.filter(item => {
+
+    return isTransitToday(item);
+
+    console.log(
+  item.resi,
+  item.batas_kirim,
+  item.ekspedisi,
+  item.batas_kirim
+);
+
+  });
+
+    if (error) {
+    console.error("Supabase Error:", error);
+    return;
+  }
+
+  const tbody = document.getElementById("tableBody");
+  tbody.innerHTML = "";
+
+  transitData
+    .slice()
+    .reverse()
+    .forEach(item => {
+      tbody.innerHTML += `
+        <tr>
+          <td>${item.resi}</td>
+          <td>${item.Pengirim}</td>
+          <td>${item.ekspedisi}</td>
+          <td>${item.status}</td>
+          <td>${item.batas_kirim}</td>
+          <td>${new Date(item.created_at).toLocaleString("id-ID")}</td>
+        </tr>
+      `;
+    });
+}
+loadOrderListTikTok();
+
+function isTransitToday(item){
+
+  const today =
+  getWIBDate(
+    new Date()
+  )
+  .toLocaleDateString("sv-SE");
+
+  // SPX & J&T
+  if(item.batas_kirim){
+
+    return item.batas_kirim === today;
+
+  }
+
+  // SiCepat
+  // if(item.ekspedisi === "SiCepat"){
+
+  //   const created =
+  //   getWIBDate(
+  //     item.created_at
+  //   );
+
+  //   const jam =
+  //     created.getHours();
+
+  //   let transitDate =
+  //     new Date(created);
+
+  //   if(jam >= 15){
+
+  //     transitDate.setDate(
+  //       transitDate.getDate() + 1
+  //     );
+
+  //   }
+
+    return (
+      transitDate
+        .toLocaleDateString("sv-SE")
+      ===
+      today
+    );
+  }
+
+  return false;
+// conversi WIB
+function getWIBDate(dateString){
+
+  return new Date(
+    new Date(dateString)
+      .toLocaleString(
+        "en-US",
+        {
+          timeZone: "Asia/Jakarta"
+        }
+      )
+  );
+
+}
+}
