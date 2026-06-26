d7d13c1e88ad4af86efaa4938662323279133c88
Monxsky <monxsky6@gmail.com>
2026-06-26 13:49:30 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -225,7 +225,34 @@ async function uploadCSV() {
   loadManifest();
 }
 
+// ==========================
+// TABS
+//===========================
+const tabs =
+document.querySelectorAll(".tab-btn");
 
+const contents =
+document.querySelectorAll(".tab-content");
+
+tabs.forEach(tab=>{
+
+    tab.addEventListener("click",()=>{
+
+        tabs.forEach(btn=>btn.classList.remove("active"));
+
+        contents.forEach(content=>
+            content.classList.remove("active")
+        );
+
+        tab.classList.add("active");
+
+        document
+        .getElementById(tab.dataset.tab)
+        .classList.add("active");
+
+    });
+
+});
 // ==========================
 // START DASHBOARD
 // ==========================
