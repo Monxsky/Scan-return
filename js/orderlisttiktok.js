84b3bc8037e04c0c4b664d6b3ad383cfd1dc2fcc
Monxsky <monxsky6@gmail.com>
2026-06-30 17:04:33 +0700

    Update orderlisttiktok.js

--- js/orderlisttiktok.js
+++ js/orderlisttiktok.js
@@ -86,5 +86,77 @@ async function loadOrderListTikTok() {
 
 }
 
+setupPagination({
+
+    table:"daftar_pesanan",
+
+    tbodyId:"tableBody",
+
+    renderRow(item){
+
+        return `
+
+            <tr>
+
+                <td>${item.marketplace_order_id}</td>
+
+                <td>${item.resi}</td>
+
+                <td>${item.ekspedisi}</td>
+
+                <td>${item.status}</td>
+
+                <td>${new Date(item.created_at)
+                    .toLocaleString("id-ID")}</td>
+
+            </tr>
+
+        `;
+
+    },
+
+    buildQuery(query){
+
+        query =
+        query.eq(
+            "marketplace",
+            "TIKTOK_ID"
+        );
+
+        if(appState.filter.status){
+
+            query =
+            query.eq(
+                "status",
+                appState.filter.status
+            );
+
+        }
+
+        if(appState.filter.expedisi){
+
+            query =
+            query.eq(
+                "ekspedisi",
+                appState.filter.expedisi
+            );
+
+        }
+
+        if(appState.filter.search){
+
+            query =
+            query.ilike(
+                "marketplace_order_id",
+                `%${appState.filter.search}%`
+            );
+
+        }
+
+        return query;
+
+    }
+
+});
 loadOrderListTikTok();
 
