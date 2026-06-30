4c4461368a86bb58aa105d2833f66798062b1a7d
Monxsky <monxsky6@gmail.com>
2026-06-30 16:07:07 +0700

    Update orderlisttiktok.js

--- js/orderlisttiktok.js
+++ js/orderlisttiktok.js
@@ -10,39 +10,57 @@ renderToolbar({
 
 });
 
-loadOrderListTikTok();
-async function loadOrderListTikTok() {
+const statusFilter = document.getElementById("statusFilter");
 
-  const { data, error } =
-    await client
-    .from("daftar_pesanan")
-    .select("*")
-    .in("marketplace", [
-      "TIKTOK_ID"
-    ]);
+if (statusFilter) {
+
+    statusFilter.addEventListener("change", function () {
+
+        loadOrderListTikTok(this.value);
+
+    });
+
+}
+
+loadOrderListTikTok();
+async function loadOrderListTikTok(status = "") {
+
+    let query = client
+        .from("daftar_pesanan")
+        .select("*")
+        .in("marketplace", ["TIKTOK_ID"]);
+
+    if (status) {
+        query = query.eq("status", status);
+    }
+
+    const { data, error } = await query;
 
     if (error) {
-    console.error("Supabase Error:", error);
-    return;
-  }
+        console.error(error);
+        return;
+    }
 
-  const tbody = document.getElementById("tableBody");
-  tbody.innerHTML = "";
+    const tbody = document.getElementById("tableBody");
+    tbody.innerHTML = "";
 
-  data
-    .slice()
-    .reverse()
-    .forEach(item => {
-      tbody.innerHTML += `
-        <tr>
-        <td>${item.marketplace_order_id}</td>
-          <td>${item.resi}</td>
-          <td>${item.ekspedisi}</td>
-          <td>${item.status}</td>
-          <td>${new Date(item.created_at).toLocaleString("id-ID")}</td>
-        </tr>
-      `;
-    });
+    data
+        .slice()
+        .reverse()
+        .forEach(item => {
+
+            tbody.innerHTML += `
+                <tr>
+                    <td>${item.marketplace_order_id}</td>
+                    <td>${item.resi}</td>
+                    <td>${item.ekspedisi}</td>
+                    <td>${item.status}</td>
+                    <td>${new Date(item.created_at).toLocaleString("id-ID")}</td>
+                </tr>
+            `;
+
+        });
+
 }
 
 loadOrderListTikTok();
