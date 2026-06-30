f165d670731bed60f844895c919483ac41305932
Monxsky <monxsky6@gmail.com>
2026-06-30 14:51:36 +0700

    Update menubar.js

--- js/menubar.js
+++ js/menubar.js
@@ -1,4 +1,43 @@
-import { createFilterBar } from "./components/filterBar.js";
+(function () {
 
-document.getElementById("filterContainer").innerHTML =
-createFilterBar();
+    function renderMenuBar(containerId = "menuBar") {
+
+        const container = document.getElementById(containerId);
+
+        if (!container) return;
+
+        container.innerHTML = `
+
+            <div class="menu-bar">
+
+                <div class="menu-item">
+
+                    <label>Status</label>
+
+                    <select id="statusFilter">
+
+                        <option value="">Semua Status</option>
+
+                        <option value="PAID">PAID</option>
+
+                        <option value="READY_TO_SHIP">READY TO SHIP</option>
+
+                        <option value="SHIPPING">SHIPPING</option>
+
+                        <option value="DELIVERED">DELIVERED</option>
+
+                        <option value="CANCELED">CANCELED</option>
+
+                    </select>
+
+                </div>
+
+            </div>
+
+        `;
+
+    }
+
+    window.renderMenuBar = renderMenuBar;
+
+})();
