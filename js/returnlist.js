b7e5e76478102d99d4544e627bba83369072a0a7
Monxsky <monxsky6@gmail.com>
2026-07-07 10:09:00 +0700

    Update returnlist.js

--- js/returnlist.js
+++ js/returnlist.js
@@ -367,7 +367,7 @@ loadPage(1);
     }
     if (overdueMode) {
         const sevenDaysAgo = new Date(
-            Date.now() - 7 * 24 * 60 * 60 * 1000
+            Date.now() - 6 * 24 * 60 * 60 * 1000
         ).toISOString();
 
         query = query
