b16f2fcd24d34e9e3900894e75c7bda3d34c41e8
Monxsky <monxsky6@gmail.com>
2026-07-09 22:08:13 +0700

    Update Analytics.js

--- js/Analytics.js
+++ js/Analytics.js
@@ -54,9 +54,9 @@ async function getOrderCount(tab, from, to) {
     query = buildDataQuery(query, from, to);
 
         query = query.in("status", [
-            "READY_TO_SHIP",
-            "SHIPPING",
-            "DELIVERED"
+            "READY_TO_SHIP"
+            // "SHIPPING",
+            // "DELIVERED"
         ]);
     if (MARKETPLACE[tab]) {
         query = query.in("marketplace", MARKETPLACE[tab]);
@@ -106,7 +106,7 @@ async function getInboundCount(tab, from, to) {
 async function getReturCount(tab, from, to) {
 
     let query = client
-        .from("v_pesanan_retur")
+        .from("pesanan_retur")
         .select("*", {
             count: "exact",
             head: true
